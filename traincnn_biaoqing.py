df59c2eaf5af425fa43d0c5b28861fc3791b1683
Xinchan233 <1060292958@qq.com>
2020-07-03 08:43:59 +0800

    dbtest

--- traincnn_biaoqing.py
+++ traincnn_biaoqing.py
@@ -1,36 +1,37 @@
 # -*- coding: utf-8 -*-
+'''
+train mnist
 
-"""
-使用ANN做情感分析
-"""
+image is grayscale with 28*28 size.
+'''
 
 # 导入包
-from oldcare.preprocessing import SimplePreprocessor
-from oldcare.datasets import SimpleDatasetLoader
+from lenet import LeNet
 from imutils import paths
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from keras.models import Sequential
-from keras.layers.core import Dense
-from keras.optimizers import SGD
-from keras.utils import to_categorical
+from keras.optimizers import SGD, Adam
+from keras.utils import to_categorical, np_utils
 import numpy as np
 import matplotlib.pyplot as plt
+# 全局常量
+from oldcare.datasets import SimpleDatasetLoader
+from oldcare.preprocessing import SimplePreprocessor
 
-# 全局变量
-dataset_path = 'images'
-accuracy_plot_path = 'plots/accuracy.png'
-loss_plot_path = 'plots/loss.png'
-output_model_path = 'models/face_expression.hdf5'
-
+LR = 0.001
+BATCH_SIZE = 128
+EPOCHS = 10
 # 全局常量
 TARGET_IMAGE_WIDTH = 28
 TARGET_IMAGE_HEIGHT = 28
-LR = 0.001  # 学习率
-BATCH_SIZE = 64
-EPOCHS = 40
 
+
+# 全局变量
+accuracy_plot_path = 'plots/accuracy.png'
+loss_plot_path = 'plots/loss.png'
+output_model_path = 'models/cnn_biaoqing.hdf5'
+dataset_path = 'images'
 ################################################
 # 第一部分：数据预处理
 
@@ -60,28 +61,25 @@ print(le.classes_)
 
 # 拆分数据集
 (X_train, X_test, y_train, y_test) = train_test_split(X, y,
-                                                      test_size=0.25,
+                                                      test_size=0.1,
                                                       random_state=42)
 
-################################################
+X_train = X_train.reshape(X_train.shape[0],28,28,1)/255
+X_test = X_test.reshape(X_test.shape[0],28,28,1)/255
+
 # 第二部分：创建并训练模型
+# initialize the optimizer and model
+print('[INFO] 编译模型...')
+opt = Adam(lr = LR)
+model = LeNet.build(28,28,2,'',1)
+model.compile(loss = 'categorical_crossentropy',
+              optimizer=opt, metrics = ['accuracy'])
 
-# 创建模型
-model = Sequential()
-model.add(Dense(1024,
-                input_shape=(TARGET_IMAGE_WIDTH * TARGET_IMAGE_HEIGHT,),
-                activation="relu"))
-model.add(Dense(512, activation="relu"))
-model.add(Dense(2, activation="softmax"))
-
-# 训练模型
-print("[INFO] 训练模型...")
-sgd = SGD(LR)
-model.compile(loss="categorical_crossentropy", optimizer=sgd,
-              metrics=["accuracy"])
-H = model.fit(X_train, y_train, validation_data=(X_test, y_test),
+# train model
+print('[INFO] 训练模型...')
+H = model.fit(X_train, y_train,
+              validation_data=(X_test, y_test),
               epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=1)
-
 
 ################################################
 # 第三部分：评估模型
@@ -89,8 +87,8 @@ H = model.fit(X_train, y_train, validation_data=(X_test, y_test),
 # 画出accuracy曲线
 plt.style.use("ggplot")
 plt.figure()
-plt.plot(np.arange(1, EPOCHS + 1), H.history["acc"], label="train_acc")
-plt.plot(np.arange(1, EPOCHS + 1), H.history["val_acc"], label="val_acc")
+plt.plot(np.arange(1, EPOCHS+1), H.history["acc"], label="train_acc")
+plt.plot(np.arange(1, EPOCHS+1), H.history["val_acc"],label="val_acc")
 plt.title("Training Accuracy")
 plt.xlabel("Epoch #")
 plt.ylabel("Accuracy")
@@ -100,8 +98,8 @@ plt.savefig(accuracy_plot_path)
 # 画出loss曲线
 plt.style.use("ggplot")
 plt.figure()
-plt.plot(np.arange(1, EPOCHS + 1), H.history["loss"], label="train_loss")
-plt.plot(np.arange(1, EPOCHS + 1), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(1,EPOCHS+1),H.history["loss"], label="train_loss")
+plt.plot(np.arange(1,EPOCHS+1),H.history["val_loss"],label="val_loss")
 plt.title("Training Loss")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss")
@@ -109,11 +107,12 @@ plt.legend()
 plt.savefig(loss_plot_path)
 
 # 打印分类报告
-label_names = le.classes_.tolist()
-print("[INFO] 评估模型...")
+# show accuracy on the testing set
+print('[INFO] 评估模型...')
 predictions = model.predict(X_test, batch_size=BATCH_SIZE)
 print(classification_report(y_test.argmax(axis=1),
-                            predictions.argmax(axis=1), target_names=label_names))
+                            predictions.argmax(axis=1), target_names=[str(i) for i in range(2)]))
+
 
 ################################################
 # 第四部分：保存模型
