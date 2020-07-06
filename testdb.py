211f8467d2afbc7fa47183245f8c89307ad7b13d
Xinchan233 <1060292958@qq.com>
2020-07-06 14:51:00 +0800

    change

--- testdb.py
+++ testdb.py
@@ -4,12 +4,11 @@ import pymysql
 #sql语句
 curr_time = datetime.datetime.now()
 db = pymysql.connect(host='123.57.246.9', user='root', password='199918', port=3306, db='oldcare')
-
 cursor = db.cursor()
 # sql语句
-sql = "insert into event_info(event_type,event_desc,event_location) value(3,'有人摔倒!!!','走廊')"
+#sql = "insert into event_info(event_type,event_desc,event_location) value(3,'有人摔倒!!!','走廊')"
 try:
-    cursor.execute(sql)
+    #cursor.execute(sql)
     print('Successful')
     db.commit()
 except:
